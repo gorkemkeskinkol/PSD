fd99cdacc30ce4a192407256f1dcb4652802b7a5
Gorkem Keskinkol <gorkemkeskinkol@gmail.com>
2023-10-09 00:02:00 +0300

    hotfix

--- Dashboard.js
+++ Dashboard.js
@@ -2,25 +2,32 @@
 
 (function($) {
     var Dashboard = {
-        version: "v1.0.6",
-        files: ["TableView.js"],
+        version: "v1.0.7",
+        files: ["TableView.js", "Sidebar.js"],
         url: function(file) {
             return `https://cdn.jsdelivr.net/gh/gorkemkeskinkol/PSD@${this.version}/${file}?nocache=1`
         },
         init: function(data) {
-            this.load_scripts()
-            $('#main_wrapper').html(Sidebar.init(data))
-        },
-        load_scripts: function() {
-            this.files.forEach(file => {
-                this.load_script(this.url(file))
+            this.load_scripts(() => {
+                $('#main_wrapper').html(Sidebar.init(data));
             });
-            this.load_script(this.url('Sidebar.js'))
         },
-        load_script: function(url) {
+        load_scripts: function(callback) {
+            let loadedCount = 0;
+            this.files.forEach(file => {
+                this.load_script(this.url(file), file, () => {
+                    loadedCount++;
+                    if (loadedCount === this.files.length) {
+                        callback();
+                    }
+                });
+            });
+        },
+        load_script: function(url, file, onSuccess) {
             $.getScript(url)
                 .done((script, textStatus) => {
                     console.log(`${file} loaded.`);
+                    onSuccess();
                 })
                 .fail((jqxhr, settings, exception) => {
                     console.error(`An error occured while loading ${file}: ${exception}`);
